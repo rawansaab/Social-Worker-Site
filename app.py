f01f3ed53650582c17fe5db6afce115968aa324c
1not2me <rawansaab777@gmail.com>
2025-11-12 01:56:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-# app.py
 # -*- coding: utf-8 -*-
+from flask import Flask, render_template, request, redirect, url_for, flash, Markup
+import os
 
-from flask import Flask, render_template, request, redirect, url_for, flash
+# צריך להגדיר את האובייקט app לפני השימוש בו
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "change-this-key"
+
 @app.before_request
 def maintenance_mode():
     if os.getenv("MAINTENANCE_MODE", "0") == "1":
@@ -43,9 +47,6 @@ def maintenance_mode():
         return Markup(html), 503
 
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "change-this-key"
-
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -53,7 +54,6 @@ def index():
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        # כאן בעתיד אפשר לחבר למייל / DB
         flash("הפנייה נשלחה בהצלחה! נחזור אליך בהקדם.", "success")
         return redirect(url_for("contact"))
     return render_template("contact.html")
@@ -61,7 +61,6 @@ def contact():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # דמו בלבד – בלי לוגיקת התחברות אמיתית.
         email = request.form.get("email")
         password = request.form.get("password")
         if email and password:
@@ -71,8 +70,6 @@ def login():
         return redirect(url_for("login"))
     return render_template("login.html")
 
-
-# נתיבי דמו לכפתורים בדף הבית
 @app.route("/students-form")
 def students_form():
     return "<h2>כאן יהיה שאלון סטודנטים (דף דמו זמני)</h2>"
